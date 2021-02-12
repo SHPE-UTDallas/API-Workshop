5deb2fbb74dc36def00ebf19a9ed4a3872267923
Gabriel Flynn <gm.flynn99@yahoo.com>
2021-02-12 12:45:02 -0600

    Setup workshop starting point

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
+const port = 3000
+
+const userDB = []
+
+app.get('/', (request, response) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`App is listening at http://localhost:${port}`)
+})
